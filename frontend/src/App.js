ba6db22ff448a3c783d5fa1d73350ad6c327ad7f
Lahindu <lahindu2001ishan@gmail.com>
2025-08-01 19:35:42 +0530

    front end done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <h1>hii</h1>
+      <h1>hii</h1>
       <h1>hii</h1>
     </div>
   );
